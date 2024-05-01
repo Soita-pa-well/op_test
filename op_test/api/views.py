f19610749e36ddfb203ee67da669efb0399dcf60
Сойта Павел <soita.pavel@yandex.ru>
2024-05-01 13:33:21 +0300

    add correct urls

--- op_test/api/views.py
+++ op_test/api/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
-# Create your views here.
+
+gauth = GoogleAuth()
+gauth.LoadCredentialsFile("mycreds.txt")
+if gauth.credentials is None:
+    print('1')
+    gauth.LocalWebserverAuth()
+elif gauth.access_token_expired:
+    print('2')
+    gauth.Refresh()
+else:
+    print('3')
+    gauth.Authorize()
+gauth.SaveCredentialsFile('credentials.txt')
+
+
+@csrf_exempt
+def create_google_drive_document(request):
+    if request.method == 'POST':
+        try:
+            data = json.loads(request.body.decode('utf-8'))
+            file_data = data.get('data')
+            file_name = data.get('name')
+            drive = GoogleDrive(gauth)
+            file = drive.CreateFile({'title': file_name})
+            file.SetContentString(file_data)
+            file.Upload()
+            return JsonResponse({'message': 'Загрузка успешна'})
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+    return JsonResponse({'error': 'Метод не определен'}, status=400)
